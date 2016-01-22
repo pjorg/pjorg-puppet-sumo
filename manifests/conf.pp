ba4ae5a10ec99ae110e0bf90028e54b04276c46d
Peter D. Jorgensen <pdjorgensen@gmail.com>
2016-01-22 16:01:16 -0500

    sumo::conf working now

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -7,7 +7,7 @@ define sumo::conf (
   $clobber = undef,
   $email = undef,
   $ephemeral = undef,
-  $name = undef,
+  $collectorName = undef,
   $override = undef,
   $password = undef,
   $proxyHost = undef,
@@ -25,15 +25,17 @@ define sumo::conf (
     fail('You must pass either an accessid/accesskey pair or an email/password pair, not both.')
   } elsif (($accessid == undef and $accesskey != undef) or ($accessid != undef and $accesskey == undef)) {
     fail('You must pass both the accessid and accesskey.')
-  } elsif (($email == undef or $password != undef) or ($email != undef or $password == undef)) {
+  } elsif (($email == undef and $password != undef) or ($email != undef and $password == undef)) {
     fail('You must pass both the email and password.')
   }
   
   file { $::sumo::params::sumo_service_config:
-    ensure  => 'present',
+    ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0600',
     content => template("${module_name}/sumo.conf.erb"),
+    require => Class['sumo::install'],
+    notify  => Class['sumo::service'],
   }
 }
--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,7 @@
 class sumo::config {
   include sumo::params
   
-  sumo::conf {'/etc/sumo.conf':
+  sumo::conf {'sumo.conf':
     accessid  => 'accessid123',
     accesskey => 'accesskey12',
     proxyHost => 'sumoproxy.lab2.local',
@@ -12,16 +12,13 @@ class sumo::config {
     syncSources => $::sumo::params::log_sync_dir,
   }
 
-  file { $::sumo::params::sumo_service_config:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    source  => 'puppet:///modules/sumo/sumo.conf',
-    require => Class['sumo::install'],
-    notify  => Class['sumo::service'],
+  file { $::sumo::params::log_sync_dir:
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
-
+  
   sumo::localfilesource {'myLocalFileSource':
     sourceName     => 'myLocalFileSourceName',
     pathExpression => '/var/log/notareallog.log',
