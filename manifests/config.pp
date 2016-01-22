69caa7b73cf6be7a720a22ca57df7ee3fce523ef
Peter D. Jorgensen <pdjorgensen@gmail.com>
2016-01-22 15:35:10 -0500

    Converted sumo.conf resource to template

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,15 @@
 #
 class sumo::config {
   include sumo::params
+  
+  sumo::conf {'/etc/sumo.conf':
+    $accessid  => 'accessid123',
+    $accesskey => 'accesskey12',
+    $proxyHost => 'sumoproxy.lab2.local',
+    $proxyPort => '8080',
+    $syncSources => $::sumo::params::log_sync_dir,
+  }
+
   file { $::sumo::params::sumo_service_config:
     ensure  => present,
     owner   => 'root',
@@ -11,12 +20,6 @@ class sumo::config {
     source  => 'puppet:///modules/sumo/sumo.conf',
     require => Class['sumo::install'],
     notify  => Class['sumo::service'],
-  }
-  file { $::sumo::params::log_sync_dir:
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 
   sumo::localfilesource {'myLocalFileSource':
