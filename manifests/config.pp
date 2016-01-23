1f61bf9a955518a3ec218e5cf6885299ff0cbc3b
Peter D. Jorgensen <pdjorgensen@gmail.com>
2016-01-23 11:15:38 -0500

    Remove syncSources from config.pp; lint and fix commas

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,30 +2,21 @@
 #
 #
 class sumo::config {
-  include sumo::params
-  
   sumo::conf {'sumo.conf':
-    accessid => $::sumo::accessid
-    accesskey => $::sumo::accesskey
-    clobber => $::sumo::clobber
-    collectorName => $::sumo::collectorName
-    email => $::sumo::email
-    ephemeral => $::sumo::ephemeral
-    override => $::sumo::override
-    password => $::sumo::password
-    proxyHost => $::sumo::proxyHost
-    proxyNtlmDomain => $::sumo::proxyNtlmDomain
-    proxyPassword => $::sumo::proxyPassword
-    proxyPort => $::sumo::proxyPort
-    proxyUser => $::sumo::proxyUser
-    sources => $::sumo::sources
-    syncSources => $::sumo::syncSources
-  }
-
-  file { $::sumo::syncSources:
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+    accessid        => $::sumo::accessid,
+    accesskey       => $::sumo::accesskey,
+    clobber         => $::sumo::clobber,
+    collectorName   => $::sumo::collectorName,
+    email           => $::sumo::email,
+    ephemeral       => $::sumo::ephemeral,
+    override        => $::sumo::override,
+    password        => $::sumo::password,
+    proxyHost       => $::sumo::proxyHost,
+    proxyNtlmDomain => $::sumo::proxyNtlmDomain,
+    proxyPassword   => $::sumo::proxyPassword,
+    proxyPort       => $::sumo::proxyPort,
+    proxyUser       => $::sumo::proxyUser,
+    sources         => $::sumo::sources,
+    syncSources     => $::sumo::syncSources,
   }
 }
