a3416961cd4ce87f90bad43e4cbf7b2a067e88b0
Peter D. Jorgensen <pdjorgensen@gmail.com>
2016-01-22 23:18:02 -0500

    Adding commas

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,21 +2,21 @@
 #
 #
 class sumo (
-  $accessid = $::sumo::params::accessid
-  $accesskey = $::sumo::params::accesskey
-  $clobber = $::sumo::params::clobber
-  $collectorName = $::sumo::params::collectorName
-  $email = $::sumo::params::email
-  $ephemeral = $::sumo::params::ephemeral
-  $override = $::sumo::params::override
-  $password = $::sumo::params::password
-  $proxyHost = $::sumo::params::proxyHost
-  $proxyNtlmDomain = $::sumo::params::proxyNtlmDomain
-  $proxyPassword = $::sumo::params::proxyPassword
-  $proxyPort = $::sumo::params::proxyPort
-  $proxyUser = $::sumo::params::proxyUser
-  $sources = $::sumo::params::sources
-  $syncSources = $::sumo::params::syncSources
+  $accessid = $::sumo::params::accessid,
+  $accesskey = $::sumo::params::accesskey,
+  $clobber = $::sumo::params::clobber,
+  $collectorName = $::sumo::params::collectorName,
+  $email = $::sumo::params::email,
+  $ephemeral = $::sumo::params::ephemeral,
+  $override = $::sumo::params::override,
+  $password = $::sumo::params::password,
+  $proxyHost = $::sumo::params::proxyHost,
+  $proxyNtlmDomain = $::sumo::params::proxyNtlmDomain,
+  $proxyPassword = $::sumo::params::proxyPassword,
+  $proxyPort = $::sumo::params::proxyPort,
+  $proxyUser = $::sumo::params::proxyUser,
+  $sources = $::sumo::params::sources,
+  $syncSources = $::sumo::params::syncSources,
 ) {
   include sumo::params, sumo::install, sumo::config, sumo::service
 }
